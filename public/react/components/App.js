839e3dfab0212d73494fbfebed64ba2daec1e171
Victoria McCrady <spartanginger@gmail.com>
2023-03-20 14:23:13 -0700

    feat(items and app): app has view state, with dummy data for testing
    now the app renders a 'Items' component, which will create a div full of cards, with all the items.
    I've created some dummy data for testing.

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
+import {Items} from './Items'
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
 export const App = () => {
 
-	const [sauces, setSauces] = useState([]);
-	const defaulView = {
+	// const [sauces, setSauces] = useState([]);
+	const defaultView = {
 		page: "home",
-		items: [],
+		items: [defaultItem,defaultItem,defaultItem],
 		slug: null,
 		item: null,
 		itemDraft: {
@@ -19,15 +20,22 @@ export const App = () => {
 			imageLink: "" 
 		}
 	}
+	const defaultItem = {
+		itemDraft: {
+			name: "Item Name",
+			price: 20,
+			description: "This item is a test item, for debugging and testing. User should never see it.",
+			imageLink: "https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2018/05/Considering-balance-in-composing-images-18.jpg?resize=717%2C478&ssl=1" 
+		}
+	}
 
-	const [view, setView] = useState(defaulView)
-
+	const [view, setView] = useState(defaultView)
+	//using this to test.
 	async function fetchSauces(){
 		try {
 			const response = await fetch(`${apiURL}/sauces`);
 			const saucesData = await response.json();
-			
-			setSauces(saucesData);
+			setView({...view, items: saucesData});
 		} catch (err) {
 			console.log("Oh no an error! ", err)
 		}
@@ -37,11 +45,13 @@ export const App = () => {
 		fetchSauces();
 	}, []);
 
-	function loader(){
+	function loader(view){
 		switch (view.page){
 			//home page
 			case 'home':
 				console.log("you are on the home view")
+				console.log(view);
+
 				return(<>
 					<h2>Items</h2>
 					<Items items={view.items}/>
@@ -51,8 +61,7 @@ export const App = () => {
 	return (
 		<main>	
       <h1>Sauce Store</h1>
-			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
+			{loader(view)}
 		</main>
 	)
 }
--- public/react/components/Items.js
+++ public/react/components/Items.js
@@ -1,1 +1,7 @@
 import React from "react";
+
+export function Items(){
+    return(<div>
+        <h1>items list needs to be built.</h1>
+    </div>);
+}

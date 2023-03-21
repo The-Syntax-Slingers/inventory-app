dbc20388931c49ff6ad6887005b32dc93dd72503
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-03-21 09:29:26 -0700

    feat: add post and update validation for non-empty fields and numeric, non-zero numbers

--- server/routes/item.js
+++ server/routes/item.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Item } = require("../models");
+const { check, validationResult } = require("express-validator");
 
 // GET /item
 router.get("/", async (req, res, next) => {
@@ -13,17 +14,29 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:id", async (req, res, next) => {
-    try {
-      const item = await Item.findByPk(req.params.id);
-      res.send(item);
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    const item = await Item.findByPk(req.params.id);
+    res.send(item);
+  } catch (error) {
+    next(error);
+  }
+});
 
 
-router.post("/", async (req, res, next) => {
+router.post("/",
+  [check("name").not().isEmpty().trim()],
+  [check("image").not().isEmpty().trim()],
+  [check("description").not().isEmpty().trim()],
+  [check("price").isNumeric().min(0.00)],
+  [check("category").not().isEmpty().trim()],
+  async (req, res, next) => {
     try {
+      // Check for validation errors
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        res.status(400).json({ error: errors.array() })
+        return
+      }
       const item = await Item.create(req.body);
       res.send(await item.findAll());
     } catch (error) {
@@ -32,16 +45,28 @@ router.post("/", async (req, res, next) => {
   });
 
 router.delete("/:id", async (req, res, next) => {
-    try {
-      const item = await Item.destroy(req.params.id);
-      res.send(item);
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    const item = await Item.destroy(req.params.id);
+    res.send(item);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.update("/:id", async (req, res, next) => {
+router.update("/:id",
+  [check("name").not().isEmpty().trim()],
+  [check("image").not().isEmpty().trim()],
+  [check("description").not().isEmpty().trim()],
+  [check("price").isNumeric().min(0.00)],
+  [check("category").not().isEmpty().trim()],
+  async (req, res, next) => {
     try {
+      // Check for validation errors
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        res.status(400).json({ error: errors.array() })
+        return
+      }
       const item = await Item.findByPk(req.params.id);
       item.update(req.body);
       res.send(item);

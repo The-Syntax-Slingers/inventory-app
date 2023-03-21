eb5da3669fa79a52cbe8cb6f19de1aa0cfa66d3e
rachelboursia <rachel.l.boursia@gmail.com>
2023-03-21 09:17:46 -0700

    feat: Adds get, post, delete, update routes

--- server/routes/item.js
+++ server/routes/item.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Item } = require("../models");
 
-// GET /sauce
+// GET /item
 router.get("/", async (req, res, next) => {
   try {
     const items = await Item.findAll();
@@ -11,5 +11,44 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+router.get("/:id", async (req, res, next) => {
+    try {
+      const item = await Item.findByPk(req.params.id);
+      res.send(item);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+
+router.post("/", async (req, res, next) => {
+    try {
+      const item = await Item.create(req.body);
+      res.send(await item.findAll());
+    } catch (error) {
+      next(error);
+    }
+  });
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+      const item = await Item.destroy(req.params.id);
+      res.send(item);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+router.update("/:id", async (req, res, next) => {
+    try {
+      const item = await Item.findByPk(req.params.id);
+      item.update(req.body);
+      res.send(item);
+    } catch (error) {
+      next(error);
+    }
+  });
+
 
 module.exports = router;

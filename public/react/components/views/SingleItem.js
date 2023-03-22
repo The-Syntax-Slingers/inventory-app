da82a87b8517e2f8f1909cf38424e7726faba463
Victoria McCrady <spartanginger@gmail.com>
2023-03-22 10:38:53 -0700

    fix(singleitem): single Item form can be updated, and doesn't loose focus after a keystroke
    #16

--- public/react/components/views/SingleItem.js
+++ public/react/components/views/SingleItem.js
@@ -13,6 +13,7 @@ export function SingleItem({ item, backToHome }) {
         }
     }
     const [formState, setFormState] = useState(defaultFormState)
+   
     //deleting item on single view
     async function deleteItem() {
         try {
@@ -35,14 +36,26 @@ export function SingleItem({ item, backToHome }) {
         console.log("submit Clicked");
     }
 
-    function UpdateItemForm({formState,setFormState}){
-        return(<>
-        <form onSubmit={handleSubmit}>
+
+return (<main>
+        <h1>{item.title}</h1>
+        <article>
+            <h3>{["$ ", item.price].join(" ")}</h3>
+            <p>{item.description}</p>
+            <img src={item.image} />
+            <br />
+            <button onClick={deleteItem}>Delete</button>
+            <button onClick={handleEdit}>Edit</button>
+            
+            {/* Form conditionally rendered below */}
+            {formState.visible ?
+            <>
+            <form onSubmit={handleSubmit}>
             {/* title; must be string */}
             <label className="hide-element" htmlFor="title">Title: </label>
             <input
                 value={formState.form.title}
-                onChange={(event)=>setFormState({...formState, form: {...form, title: event.target.value}})}
+                onChange={(event)=>setFormState({...formState, form: {...formState.form, title: event.target.value}})}
                 type="text"
                 name="title"
             ></input>
@@ -53,7 +66,7 @@ export function SingleItem({ item, backToHome }) {
                 value={formState.form.price}
                 onChange={(event)=>{
                     console.log("form state is ",formState.form);
-                    setFormState({...formState, form: {...form, price: event.target.value}});
+                    setFormState({...formState, form: {...formState.form, price: event.target.value}});
                 }}
                 type="number"
                 min="0"
@@ -65,7 +78,7 @@ export function SingleItem({ item, backToHome }) {
              <label className="hide-element" htmlFor="description" >Description: </label>
             <input
                 value={formState.form.description}
-                onChange={(event)=>setFormState({...formState, form: {...form, description: event.target.value}})}
+                onChange={(event)=>setFormState({...formState, form: {...formState.form, description: event.target.value}})}
                 type="text"
                 name="description"
             ></input>   
@@ -74,7 +87,7 @@ export function SingleItem({ item, backToHome }) {
             <label className="hide-element" htmlFor="image" >Image: </label>
             <input
                 value={formState.form.image}
-                onChange={(event)=>setFormState({...formState, form: {...form, image: event.target.value}})}
+                onChange={(event)=>setFormState({...formState, form: {...formState.form, image: event.target.value}})}
                 type="text"
                 name="image"
             ></input>     
@@ -83,30 +96,26 @@ export function SingleItem({ item, backToHome }) {
             <label className="hide-element" htmlFor="category" ></label>
             <input
                  value={formState.form.category}
-                 onChange={(event)=>setFormState({...formState, form: {...form, category: event.target.value}})}
+                 onChange={(event)=>setFormState({...formState, form: {...formState.form, category: event.target.value}})}
                  type="text"
                  name="description"
             ></input>
 
+            
+            <div>
             <label className="hide-element" htmlFor="submit" >Submit: </label>
-            <input type="submit"></input>
+            <input type="submit" name="submit"></input>
+
+            <label className="hide-element" htmlFor="cancel" >Cancel: </label>
+            <button onClick={(event)=>{event.preventDefault();console.log("Cancel was clicked!")}} name="cancel">Cancel</button>
+            </div>
+            
+
 
             <label></label>
-            <button onClick={console.log("write cancel function")}>cancel</button>
                 
-        </form></>);
-    }
-    return (<main>
-        <h1>{item.title}</h1>
-        <article>
-            <h3>{["$ ", item.price].join(" ")}</h3>
-            <p>{item.description}</p>
-            <img src={item.image} />
-            <br />
-            <button onClick={deleteItem}>Delete</button>
-            <button onClick={handleEdit}>Edit</button>
-            {formState.visible ?
-              <UpdateItemForm formState={formState} setFormState={setFormState} />
+        </form>
+            </>
               :
               ""
             }

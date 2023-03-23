045fd884dff6695cc16494a0f7a2cb09cdbdd564
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-03-22 21:24:03 -0700

    feat: style app logo (Inventory App)

--- public/react/components/NavBar.js
+++ public/react/components/NavBar.js
@@ -3,7 +3,10 @@ import React from "react";
 export function NavBar({ view, navClicks }) {
     console.log("navbar says view is ", view)
     return (<nav>
-        <h2 className="nav-item" onClick={navClicks.home}>Inventory App</h2>
+        <div className="app-logo">
+            <span className="nav-item emoji">📇</span>
+            <h2 className="nav-item app-logo" onClick={navClicks.home}>Inventory App</h2>
+        </div>
         <ul>
             <a className="nav-item nav-link" onClick={navClicks.home}>
                 Home

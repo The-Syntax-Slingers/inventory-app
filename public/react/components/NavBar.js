4bbd647de3e0c739f06c529bfc30a600a625edfa
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-03-22 17:38:49 -0700

    feat: Inventory App logo in Nav onClick to home page

--- public/react/components/NavBar.js
+++ public/react/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 export function NavBar({ view, navClicks }) {
     console.log("navbar says view is ", view)
     return (<nav>
-        <h2 className="nav-item">Inventory App</h2>
+        <h2 className="nav-item" onClick={navClicks.home}>Inventory App</h2>
         <ul>
             <a className="nav-item nav-link" onClick={navClicks.home}>
                 Home

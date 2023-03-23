5490c49fb6ebfd7675df670ee769294e393089b3
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-03-22 21:31:20 -0700

    ammend commit

--- public/react/components/NavBar.js
+++ public/react/components/NavBar.js
@@ -3,9 +3,9 @@ import React from "react";
 export function NavBar({ view, navClicks }) {
     console.log("navbar says view is ", view)
     return (<nav>
-        <div className="app-logo">
+        <div className="app-logo" onClick={navClicks.home}>
             <span className="nav-item emoji">📇</span>
-            <h2 className="nav-item app-logo" onClick={navClicks.home}>Inventory App</h2>
+            <h2 className="nav-item app-logo" >Inventory App</h2>
         </div>
         <ul>
             <a className="nav-item nav-link" onClick={navClicks.home}>
